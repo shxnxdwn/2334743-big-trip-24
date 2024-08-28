a50f3bc1cd8a96bdd7549ac42057eea39a2d407f
Gleb Kornilov <esplete@gmail.com>
2024-08-28 04:05:27 +0300

    feat: add presenters for add and edit modules

---
+++ src/presenter/add-point-presenter.js
@@ -0,0 +1,11 @@
+import DestinationAddView from '../view/add-point/components/destination-add-view.js';
+import DetailAddView from '../view/add-point/components/detail-add-view.js';
+import InfoAddView from '../view/add-point/components/info-add-view.js';
+import OfferListAddView from '../view/add-point/components/offer-list-add-view.js';
+import { render } from '../render.js';
+
+export default class AddPointPresenter {
+  constructor() {
+
+  }
+}
---
+++ src/presenter/edit-point-presenter.js
@@ -0,0 +1,11 @@
+import DestinationEditView from '../view/edit-point/components/destination-edit-view.js';
+import DetailEditView from '../view/edit-point/components/detail-edit-view.js';
+import InfoEditView from '../view/edit-point/components/info-edit-view.js';
+import OfferListEditView from '../view/edit-point/components/offer-list-edit-view.js';
+import { render } from '../render.js';
+
+export default class EditPointPresenter {
+  constructor() {
+
+  }
+}
